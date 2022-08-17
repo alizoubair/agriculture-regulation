220c2f4bfa426a031680c7e7bbb08c90ed51d7a9
alizoubair <ali.zoubair@outlook.com>
2022-08-17 14:57:39 +0100

    Add createGreenhouse action method

--- app/Http/Controllers/Admin/AdminFarmController.php
+++ app/Http/Controllers/Admin/AdminFarmController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Farm;
+use APp\Models\Greenhouse;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Mapper;
@@ -23,6 +24,18 @@ class AdminFarmController extends Controller
 		$newFarm->setPerimeter($request->input('perimeter'));
 		$newFarm->setArea($request->input('area'));
 		$newFarm->save();
+
+		return back();
+	}
+
+	public function createGreenhouse(Request $request)
+	{
+		$newGreenhouse = new Greenhosue();
+		$newGreenhouse->setName($request->input(name));
+		$newGreenhouse->setFarmId($request->input('farm_id'));
+		$newGreenhouse->setPerimeter($request->input('perimeter'));
+		$newGreenhouse->setArea($request->input('area'));
+		$newGreenhouse->save();
 
 		return back();
 	}
