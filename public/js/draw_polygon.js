f727bb067fb96d5430c74399af854208b86a8957
alizoubair <ali.zoubair@outlook.com>
2022-09-10 18:30:11 +0100

    Add delete and render popups

--- public/js/draw_polygon.js
+++ public/js/draw_polygon.js
@@ -1,6 +1,11 @@
 import { map } from "./mapbox.js";
 import * as utils from './utils.js';
 
+var cancelPop = "<button id='cancelPopup'>Annuler</button>";
+var renderPop = "<button id='renderPopup'>Terminer</button>";
+var vertex = 0, last_vertex = 0;
+var coordinates, removePopup, renderPopup, areaPopup;
+
 utils.fitView();
 
 /* Overriding polygon features */
@@ -23,7 +28,6 @@ export const draw = new MapboxDraw({
     // Select which mapbox-gl-draw control buttons to add to the map.
     controls: {
         polygon: true,
-        trash: true
     },
     // Set mapbox-gl-draw to draw by default.
     // The user does not have to click the polygon control button first.
@@ -37,62 +41,56 @@ map.on('draw.delete', updateArea);
 map.on('draw.update', updateArea);
 
 /* Delete last vertex drawn of polygon */
-var cancelPop = "<button id='cancelPopup'>Annuler</button>"
-var renderPop = "<button id='renderPopup'>Terminer</button>"
-let vertex = 0;
-var popup;
-var is_same = false;
-var last_vertex = 0;
-
-map.on('click', (e) => {
+function removePop(last_vertex) {
     var list = draw.getAll().features[0].geometry.coordinates[0];
-    var coords = Object.values(list.reduce((p, c) => (p[JSON.stringify(c)] = c, p), {}));
+    coordinates = Object.values(list.reduce((p, c) => (p[JSON.stringify(c)] = c, p), {}));
+
+    if (last_vertex == 0) {
+        return;
+    }
+
+    if (coordinates.length <= 3) {
+        renderPopup.remove();
+        areaPopup.remove();
+    }
+
+    removePopup = new mapboxgl.Popup({ closeButton: false, className: "cancelPopup" })
+        .setLngLat(coordinates[last_vertex])
+        .setHTML(cancelPop)
+        .on('open', e => {
+            if (document.getElementById('cancelPopup')) {
+                document.getElementById('cancelPopup')
+                    .addEventListener('click', e => {
+                        draw.trash();
+                        removePopup.remove();
+                        last_vertex = vertex - 2;
+
+                        removePop(last_vertex);
+
+                        vertex--;
+                    });
+            }
+        })
+        .addTo(map);
+}
+
+map.on('click', () => {
+    var list = draw.getAll().features[0].geometry.coordinates[0];
+    coordinates = Object.values(list.reduce((p, c) => (p[JSON.stringify(c)] = c, p), {}));
 
     if (vertex) {
-        popup = new mapboxgl.Popup({ closeButton: false, className: "cancelPopup" })
-            .setLngLat(coords[vertex])
+        removePopup = new mapboxgl.Popup({ closeButton: false, className: "cancelPopup" })
+            .setLngLat(coordinates[vertex])
             .setHTML(cancelPop)
             .on('open', e => {
                 if (document.getElementById('cancelPopup')) {
                     document.getElementById('cancelPopup')
                         .addEventListener('click', e => {
                             draw.trash();
-                            popup.remove();
-                            var last_vertex = vertex - 2;
+                            removePopup.remove();
+                            last_vertex = vertex - 2;
 
-                            popup = new mapboxgl.Popup({ closeButton: false, className: "cancelPopup" })
-                                .setLngLat(coords[last_vertex])
-                                .setHTML(cancelPop)
-                                .on('open', e => {
-                                    if (document.getElementById('cancelPopup')) {
-                                        document.getElementById('cancelPopup')
-                                            .addEventListener('click', e => {
-                                                draw.trash();
-                                                popup.remove();
-                                                last_vertex = vertex - 2;
-
-                                                popup = new mapboxgl.Popup({ closeButton: false, className: "cancelPopup" })
-                                                    .setLngLat(coords[last_vertex])
-                                                    .setHTML(cancelPop)
-                                                    .on('open', e => {
-                                                        if (document.getElementById('cancelPopup')) {
-                                                            document.getElementById('cancelPopup')
-                                                                .addEventListener('click', e => {
-                                                                    draw.trash();
-                                                                    popup.remove();
-                                                                    last_vertex = vertex - 2;
-
-                                                                    vertex--;
-                                                                });
-                                                        }
-                                                    })
-                                                    .addTo(map);
-
-                                                vertex--;
-                                            });
-                                    }
-                                })
-                                .addTo(map);
+                            removePop(last_vertex);
 
                             vertex--;
                         });
@@ -100,6 +98,7 @@ map.on('click', (e) => {
             })
             .addTo(map);
     }
+
     vertex++;
 });
 
@@ -107,30 +106,32 @@ map.on('click', () => {
     const data = draw.getAll();
     const Id = draw.getAll().features[0].id;
     var list = draw.getAll().features[0].geometry.coordinates[0];
-    var coords = Object.values(list.reduce((p, c) => (p[JSON.stringify(c)] = c, p), {}));
+    var coordinates = Object.values(list.reduce((p, c) => (p[JSON.stringify(c)] = c, p), {}));
     const center = turf.center(data.features[data.features.length - 1]).geometry.coordinates;
 
-    if (coords.length > 2) {
+    if (coordinates.length > 2) {
         var dimensions = utils.getDimensions(data, data.features[data.features.length - 1].geometry.coordinates);
         const updated_area = (dimensions[0] < 1000) ? `${dimensions[0]} m²`
             : `${dimensions[0] / 1000000} km²`;
-console.log(dimensions[0])
-console.log(updated_area)
+
         // Add a popup to display surface
-        new mapboxgl.Popup({ closeButton: false, className: 'area' })
+        areaPopup = new mapboxgl.Popup({ closeButton: false, className: 'areaPopup' })
             .setLngLat(center)
             .setHTML(`surface: ${updated_area}`)
             .addTo(map)
 
         // Add a popup to render geometry
-        new mapboxgl.Popup({ closeButton: false, className: "renderPopup" })
-            .setLngLat(coords[0])
+        renderPopup = new mapboxgl.Popup({ closeButton: false, className: "renderPopup" })
+            .setLngLat(coordinates[0])
             .setHTML(renderPop)
             .on('open', e => {
                 if (document.getElementById('renderPopup')) {
                     document.getElementById('renderPopup')
                         .addEventListener('click', e => {
                             draw.changeMode('direct_select', { featureId: `${Id}` });
+                            renderPopup.remove();
+                            removePopup.remove();
+                            areaPopup.remove();
                         })
                 }
             })
